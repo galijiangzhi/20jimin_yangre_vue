bdc9a5021820bf57e095b21d5370b2e19444a68e
baizhen <galijiangzhi@163.com>
2024-04-03 09:14:56 +0800

    test

--- houduan/main.py
+++ houduan/main.py
@@ -3,6 +3,7 @@ import os
 from flask_cors import CORS
 import time
 import shutil
+import re
 
 app = Flask(__name__)
 CORS(app)  # 启用CORS，允许所有源
@@ -11,8 +12,32 @@ CORS(app)  # 启用CORS，允许所有源
 UPLOAD_FOLDER = '/home/baizhen/project/6.jpg'
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
+@app.route('/lmj', methods=['POST'])
+def lmj():
+    #lmj
+    if 'file' not in request.files:
+        return 'No file part', 400
+    file = request.files['file']
+    if file.filename == '':
+        return 'No selected file', 400
+    if file:
+        file_path = '/home/baizhen/project/lmj_ABINet_lock/data/testset/6.jpg'
+        file.save(file_path)  # 保存文件
+        # 执行脚本
+        print("开始执行脚本")
+        os.system("python /home/baizhen/project/lmj_ABINet_lock/demo_bz.py")
+        print("脚本执行完成")
+        test_txt = '/home/baizhen/project/lmj_ABINet_lock/workdir/train-abinet/test.txt'
+        with open(test_txt, 'r') as file:
+            txt = file.readlines()[-1]
+        print(txt)
+        txt = re.search(r':\s*(.*)', txt).group(1)
+        print(txt)
+        return txt
+
 @app.route('/upload', methods=['POST'])
 def upload_file():
+    #yyrr
     if 'file' not in request.files:
         return 'No file part', 400
     file = request.files['file']
